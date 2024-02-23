a59edd8479422da9e0f46189cc17de3b66c9fe2a
Bhav Beri <bhavberi@gmail.com>
2024-02-23 15:08:27 +0530

    List files - for testing

--- openai_refactor.py
+++ openai_refactor.py
@@ -1,3 +1,16 @@
 import openai
+import os
 
 print("Hello, OpenAI!")
+
+def list_files_recursive(directory):
+    for root, dirs, files in os.walk(directory):
+        level = root.replace(directory, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        sub_indent = ' ' * 4 * (level + 1)
+        for file in files:
+            print('{}{}'.format(sub_indent, file))
+
+# Call the function with the desired directory
+# list_files_recursive('./')
